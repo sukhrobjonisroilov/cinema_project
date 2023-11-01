d3461c564bb1a7c01e3ac9f4dd111a848884f741
Sukhrobjon Isroilov <106602969+sukhrobjonisroilov@users.noreply.github.com>
2023-11-01 15:30:27 +0500

    views
    views

--- core/cinema/views.py
+++ core/cinema/views.py
@@ -1,10 +1,8 @@
-from  rest_framework import generics
-
-
-
+from rest_framework import generics
 
 from core.cinema.serialezrs import GenreSerializer
 from core.cinema.models import Genre
+
 
 class GenreView(generics.ListAPIView):
     queryset = Genre.objects.all()
